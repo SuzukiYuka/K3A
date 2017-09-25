a00bb65dafde1a4be43cc8c104832da95e24af8b
Yuka Suzuki <suzukiyuka1218@gmail.com>
2017-09-25 10:55:47 +0900

    add objects

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerController : MonoBehaviour {
+    float speed = 10.0f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+        if (Input.GetKey(KeyCode.RightArrow)) {
+            
+            transform.position += new Vector3(speed*Time.deltaTime, 0, 0);
+        }
+		if (Input.GetKey(KeyCode.LeftArrow)) {
+
+			transform.position += new Vector3(-speed * Time.deltaTime, 0, 0);
+		}
+		if (Input.GetKey(KeyCode.UpArrow)) {
+
+			transform.position += new Vector3(0, speed * Time.deltaTime, 0);
+		}
+
+        if (transform.position.y <= -10) {
+
+            SceneManager.LoadScene("GameOver");
+        }
+	}
+}
