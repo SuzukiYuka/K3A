9c86bb7b1bf7886b3654359cfc6ae01962fa562e
Yuka Suzuki <suzukiyuka1218@gmail.com>
2017-09-29 10:57:07 +0900

    wip animation #8

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour {
 
@@ -10,82 +9,39 @@ public class PlayerController : MonoBehaviour {
     float jumpSpeed = 200f;
     float groundLine = 20.0f;
     float jumpPower = 400.0f;
-    Rigidbody playerRigidBody;
     int jumpCount = 0;
     int coinCount = 0;
+
+    Animator animator;
 
     bool isJump {
 
         get { return jumpCount != 0; }
     }
 
-    public Text coinLabel;
-
     void Start() {
 
-        playerRigidBody = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
     }
 
     void Update() {
-
-        if (Input.GetKey(KeyCode.D)) {
-
-            if (!isJump) {
-
-                playerRigidBody.velocity = new Vector3(speed, 0, 0);
-            }
-        }
-
-        if (Input.GetKey(KeyCode.A)) {
-
-            if (!isJump) {
-
-                playerRigidBody.velocity = new Vector3(-speed, 0, 0);
-            }
-        }
 
         if (Input.GetKeyDown(KeyCode.Space)) {
 
             if (!isJump) {
 
                 jumpCount += 1;
-                playerRigidBody.AddForce(Vector3.up * jumpPower);
+                animator.SetBool("Jump", true);
             }
+        } else {
+
+            animator.SetBool("Jump", false);
         }
 
-        if (Input.GetKeyDown(KeyCode.D)) {
-
-            if (isJump) {
-
-                playerRigidBody.AddForce(Vector3.right * jumpSpeed);
-            }
-        }
-
-        if (Input.GetKeyDown(KeyCode.A)) {
-            if (isJump) {
-
-                playerRigidBody.AddForce((Vector3.left * jumpSpeed));
-            }
-        }
 
         if (transform.position.y <= -groundLine) {
 
             SceneManager.LoadScene("GameOver");
-        }
-    }
-
-    private void OnCollisionEnter(Collision collision) {
-
-        if (collision.gameObject.CompareTag("Stage")) {
-
-            jumpCount = 0;
-        }
-
-        if (collision.gameObject.CompareTag("Coin")) {
-
-            coinCount++;
-            coinLabel.text = coinCount.ToString();
-            Destroy(collision.collider.gameObject);
         }
     }
 }
