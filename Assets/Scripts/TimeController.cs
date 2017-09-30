7b7d9de3068e463b5664e2b8372f1154189ce628
Yuka Suzuki <suzukiyuka1218@gmail.com>
2017-09-30 17:49:19 +0900

    add timer #1

---
+++ Assets/Scripts/TimeController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeController : MonoBehaviour {
+
+    public Text timeLabel;
+
+    float time = 0.0f;
+
+    void Update() {
+
+        time += Time.deltaTime;
+        timeLabel.text = time.ToString("F2");
+    }
+}
